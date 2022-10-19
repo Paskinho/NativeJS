55e85b0305d7b8304d36a6ce0f6441213d29644b
paskanov <v.paskanov@gmail.com>
2022-10-19 11:34:28 +0300

    Test

--- src/01-hello-tests/01.ts
+++ src/01-hello-tests/01.ts
@@ -1,4 +1,12 @@
 
+export function sum(a: number, b: number) {
+    return a+b
+}
 
+export function mult(a:number, b:number){
+    return a * b
+}
 
-export const value01_01 - "01_01"
+export function splitIntoWords(sentense: string) {
+    return ['hello', 'my', 'friend']
+}
