0251eec80d1d8e2a81b2ca19a433c8747c35e02e
paskanov <v.paskanov@gmail.com>
2022-10-26 15:43:07 +0300

    logic filter

---
+++ src/04/04.ts
@@ -0,0 +1,30 @@
+
+const ages = [10, 20, 22, 1, 100, 90, 14];
+
+const predicate = (age: number) => {
+    return age > 90;
+}
+
+const odlAges = [100];
+
+type courseType = {
+title: string
+    price: number
+}
+
+
+const courses = [
+    {title: 'CSS', price: 100},
+    {title: 'JS', price: 200},
+    {title: 'REACT', price: 150}
+
+]
+
+const chipPredicate = (course:courseType) => {
+return course.price < 160
+}
+
+const chip Course = [
+    {title: 'CSS', price: 100},
+    {title: 'REACT', price: 150}
+]
