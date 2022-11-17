bdbde68d08981f3aef21f5cf41b59a9a8041180c
paskanov <v.paskanov@gmail.com>
2022-11-17 16:41:59 +0300

    09 lesson - testing

---
+++ src/09/09_01_test.tsx
@@ -0,0 +1,28 @@
+
+function increaseAge (u: UserType) {
+    u.age++;
+}
+
+type UserType = {
+    name: string
+    age: number
+}
+
+
+test ('big test', ()=> {
+    const user = {
+        name: 'Dimych',
+        age: 32
+    }
+    increaseAge(user);
+
+    expect(user.age).toBe(33)
+
+    const superman = user
+
+    superman.age = 1000
+
+    expect(user.age).toBe(1000)
+
+
+    })
