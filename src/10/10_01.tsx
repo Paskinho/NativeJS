e19da1fa8180da58f280a9c04c53ddde6f5a2d06
paskanov <v.paskanov@gmail.com>
2022-11-25 09:38:25 +0300

    refactoring 10 lesson

--- src/10/10_01.tsx
+++ src/10/10_01.tsx
@@ -1,9 +1,40 @@
 
-
 export type UserType= {
     name: string,
     hair: number,
-    address: {title: string}
-
+    address: { city: string, house: number}
 }
+
+    export type LaptopType = {
+        title: string
+}
+
+export  type UserWithLaptopType = UserType & {
+    laptop: LaptopType
+}
+
+
+export function makeHairstyle (u: UserType, power: number) {
+    const copy = {
+        ...u,
+        hair: u.hair/power
+    }
+    // copy.hair = u.hair/power
+    return copy
+}
+
+
+export function moveUser (u: UserWithLaptopType, city : string) {
+    const copyMoveUser = {
+        ...u,
+        address: {
+            ...u.address,
+            city: "Kiev"
+        }
+    }
+    // copyMoveUser.address = {...copyMoveUser.address}
+    return copyMoveUser
+}
+
+
 
--- src/10/10_01_test.tsx
+++ src/10/10_01_test.tsx
@@ -1,14 +1,6 @@
-import {UserType} from "./10_01";
-
-
-function makeHairstyle (u: UserType, power: number) {
-    const copy = {
-        ...u
-    }
-    copy.hair = u.hair/power
-    return copy
-}
-
+import {UserType, UserWithLaptopType} from "./10_01";
+import {makeHairstyle} from "./10_01";
+import {moveUser} from "./10_01";
 
 test ('reference type test', ()=> {
     let user: UserType =
@@ -16,16 +8,66 @@ test ('reference type test', ()=> {
         name: 'Dimych',
         hair: 32,
             address: {
-            title: "Minsk"
+            city: "Minsk",
+                house:12
             }
     }
 
         const awesomeHair =  makeHairstyle(user,2);
 
 
+    user.address.city= "Kiev"
+
         expect(awesomeHair.hair).toBe(16)
     expect(user.hair).toBe(16)
-
-
+    expect(awesomeHair.address).toBe(user.address)
 }
 )
+
+test ('change address', ()=> {
+    let user: UserType =
+        {
+            name: 'Dimych',
+            hair: 32,
+            address: {
+                city: "Minsk",
+                house: 16
+            }
+        }
+
+        const awesomeHair = makeHairstyle(user, 2)
+       user = awesomeHair;
+
+        expect(awesomeHair.hair).toBe(16)
+        expect(user.hair).toBe(32)
+        expect(awesomeHair.address).toBe(user.address)
+
+})
+
+test ('change address', ()=> {
+    let user: UserWithLaptopType =
+        {
+            name: 'Dimych',
+            hair: 32,
+            address: {
+                city: "Minsk",
+                house: 14
+            },
+            laptop: {
+                title: "ZenBook"
+            }
+        }
+
+    let movedUser = moveUser(user, "Kiev")
+
+
+
+    expect(user).not.toBe(movedUser)
+    expect(user.address).not.toBe(movedUser.address)
+    expect(user.laptop).toBe(movedUser.laptop)
+    expect(movedUser.address.city).toBe("Kiev")
+
+})
+
+
+
