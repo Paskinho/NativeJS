b362e54bebccc0ee7a935791c6183c5ea0921be7
paskanov <v.paskanov@gmail.com>
2023-02-21 09:29:29 +0300

    testing promises. Resolve message

--- src/promises/promises-14lesson.js
+++ src/promises/promises-14lesson.js
@@ -1,21 +1,18 @@
 let axios = {
     get() {
-        let pr = new Promise ((resolve, reject) => {
+        return new Promise ((resolve, reject) => {
             setTimeout(()=> {
-                resolve('YoYoYo')
+                resolve({G: 2, g3: 4, message: "YoYoyo"})
             },3000)
         });
-        return pr
     }
 }
-
-
 let promise = axios.get();
 
 promise.then((message)=> {
-    console.log(message)
+    return 1
 })
-    .then()
+    .then((message)=> console.log(message))
     .then()
     .then()
     .then();
